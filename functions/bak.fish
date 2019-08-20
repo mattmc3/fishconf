b33136fe9b6c9c94436298b9e2724cb8e49b8d12
mattmc3 <mattmc3@gmail.com>
2019-08-19 22:56:21 -0400

    Added bak function for easy file backup copies

---
+++ functions/bak.fish
@@ -0,0 +1,10 @@
+function bak --description 'backup a file'
+    set -l now (date +"%Y%m%d-%H%M%S")
+    for f in $argv
+        if not test -e "$f"
+            echo "file not found: $f" >&2
+            continue
+        end
+        cp -R "$f" "$f".$now
+    end
+end
