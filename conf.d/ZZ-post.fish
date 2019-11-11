3b2381f722e7643a420fbe1983e95a17ec3452f7
mattmc3 <mattmc3@gmail.com>
2019-11-11 13:47:33 -0500

    Fix local settings

--- conf.d/ZZ-post.fish
+++ conf.d/ZZ-post.fish
@@ -1,2 +1,13 @@
-set fish_function_path $DOTFILES.local/fish/functions.local $fish_function_path
-[ -f $DOTFILES.local/fish/config.local.fish ] ;and source $DOTFILES.local/fish/config.local.fish
+# local settings
+
+if test -d $HOME/.config/fish/functions.local
+    set fish_function_path $HOME/.config/fish/functions.local $fish_function_path
+else if test -d $DOTFILES/local/fish/fish/functions.local
+    set fish_function_path $DOTFILES/local/fish/fish/functions.local $fish_function_path
+end
+
+if test -f $HOME/.config/fish/config.local.fish
+    source $HOME/.config/fish/config.local.fish
+else if test -f $DOTFILES/local/fish/fish/config.local.fish
+    source $DOTFILES/local/fish/fish/config.local.fish
+end
