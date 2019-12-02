d8ab3b60113d9535f1c7b9d289994128d054688f
mattmc3 <mattmc3@gmail.com>
2019-12-02 10:32:42 -0500

    Add gacp function

---
+++ functions/gacp.fish
@@ -0,0 +1,27 @@
+function gacp --description 'git add/commit/push all-in-one'
+    set curbranch (git rev-parse --abbrev-ref HEAD)
+    if test $status -ne 0
+        echo "gacp: Not in a git repo" >&2
+        return 1
+    end
+    if test (count $argv) -lt 1
+        echo "gacp: Expecting commit message" >&2
+        return 1
+    end
+    if curbranch == "master"
+        while true
+            read -l -P "You are about to add/commit/push to master. Are you sure? [y/N] " confirm
+            switch $confirm
+                case Y y
+                    break
+                case '' N n
+                    return 1
+            end
+        end
+    end
+
+    git add .
+    git add --all
+    git commit -am "$argv"
+    git push
+end
