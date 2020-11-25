57992c6f523c6ee5c9d3db50204b580777b5a839
mattmc3 <mattmc3@gmail.com>
2020-11-25 11:35:10 -0500

    Use the cd-ls plugin instead of cd-ls in conf.d

--- conf.d/cd-ls.fish
+++
@@ -1,8 +0,0 @@
-set -q LS_AFTER_CD || set -xg LS_AFTER_CD true
-
-# show directory listing on directory change
-function __ls_after_cd__on_variable_pwd --on-variable PWD
-    if test "$LS_AFTER_CD" = true; and status --is-interactive
-        ls -GF
-    end
-end
