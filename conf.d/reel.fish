093279871e25ed829ac628fb33f5ac1ddc53bda8
mattmc3 <mattmc3@gmail.com>
2020-12-20 17:30:10 -0500

    Change reel plugins

--- conf.d/reel.fish
+++ conf.d/reel.fish
@@ -1,24 +1,33 @@
 # ~/.config/fish/conf.d/reel.fish
 
+# get reel if we don't have it
+if not test -d $__fish_config_dir/plugins/mattmc3/reel
+    git clone --depth 1 https://github.com/mattmc3/reel $__fish_config_dir/plugins/mattmc3/reel
+end
+
 # make the reel function available
-contains ~/.config/fish/plugins/mattmc3/reel/functions $fish_function_path
-or set fish_function_path ~/.config/fish/plugins/mattmc3/reel/functions $fish_function_path
+contains $__fish_config_dir/plugins/mattmc3/reel/functions $fish_function_path
+or set fish_function_path $__fish_config_dir/plugins/mattmc3/reel/functions $fish_function_path
 
 # store your plugins in a fish_plugins variable for ease
 set fish_plugins \
-    "decors/fish-colored-man" \
-    "edc/bass" \
-    "fishingline/cd-ls" \
-    "fishingline/common-funcs" \
-    "jethrokuan/z" \
-    "fishingline/gitdot" \
-    "fishingline/macos" \
-    "fishingline/python" \
-    "fishingline/safe-rm" \
-    # "fishingline/sorin-prompt-enhanced" \
-    "fishingline/up" \
-    "laughedelic/fish_logo" \
-    "rafaelrinaldi/pure"
+    mattmc3/reel \
+    mattmc3/up.fish \
+    mattmc3/iwd.fish \
+    mattmc3/cd-ls.fish \
+    mattmc3/macos.fish \
+    decors/fish-colored-man \
+    edc/bass \
+    fishingline/cd-ls \
+    fishingline/common-funcs \
+    jethrokuan/z \
+    fishingline/gitdot \
+    fishingline/macos \
+    fishingline/python \
+    fishingline/safe-rm \
+    # fishingline/sorin-prompt-enhanced \
+    # rafaelrinaldi/pure \
+    laughedelic/fish_logo
 
 # reel in (clone+load) your plugins
 reel in $fish_plugins
