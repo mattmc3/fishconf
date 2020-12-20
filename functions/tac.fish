633c23fe92ca1e8aced32731be36af980647a00d
mattmc3 <mattmc3@gmail.com>
2020-12-20 18:55:50 -0500

    Support for tac utility

--- functions/tac.fish
+++ functions/tac.fish
@@ -1,3 +1,7 @@
 function tac --description 'Reverse cat'
-    cat $argv | awk '{a[NR]=$0}END{for(i=NR;i>0;i--)print a[i]}'
+    if type -q tac
+        tac $argv
+    else
+        cat $argv | awk '{a[NR]=$0}END{for(i=NR;i>0;i--)print a[i]}'
+    end
 end
