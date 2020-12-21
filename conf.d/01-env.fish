593ed59bcaee6a460cf7fecc94439901705e44f3
mattmc3 <mattmc3@gmail.com>
2020-12-21 00:21:11 -0500

    Fix dotfiles path

--- conf.d/01-env.fish
+++ conf.d/01-env.fish
@@ -1,8 +1,5 @@
 # dotfiles
-set -q DOTFILES; or set -gx DOTFILES ~/.dotfiles
-
-# it's nice to have a variable for where the fish config lives
-set -q fish_config; or set -gx fish_config $HOME/.config/fish
+set -q DOTFILES; or set -gx DOTFILES ~/.config/dotfiles
 
 # path
 set -gx PATH \
@@ -40,11 +37,11 @@ set -gx PATH $HOME/bin $PATH $XDG_DATA_HOME/npm/bin
 set fish_function_path $XDG_CONFIG_HOME/fish/aliases $fish_function_path
 
 # add function subdirs to fish_function_path
-for d in $fish_config/functions/*/
+for d in $__fish_config_dir/functions/*/
     set fish_function_path $d $fish_function_path
 end
 
 # add completion subdirs to fish_function_path
-for d in $fish_config/completions/*/
+for d in $__fish_config_dir/completions/*/
     set fish_complete_path $d $fish_complete_path
 end
