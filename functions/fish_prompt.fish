ca742bb016172e263b0ae335d85f3196cf270897
mattmc3 <mattmc3@gmail.com>
2020-12-21 23:21:16 -0500

    Pure prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,14 +1,14 @@
 function fish_prompt
-    if test -n "$SSH_TTY"
-        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
-    end
+    set --local exit_code $status # save previous exit code
 
-    echo -n (set_color blue)(prompt_pwd)' '
-
-    set_color -o
-    if test "$USER" = 'root'
-        echo -n (set_color red)'# '
+    echo -e -n (_pure_prompt_beginning) # init prompt context (clear current line, etc.)
+    echo -e (_pure_prompt_first_line) # print current path, git branch/status, command duration
+    _pure_place_iterm2_prompt_mark # place iTerm shell integration mark
+    if test $exit_code -ne 0
+        echo -e -n (set_color red)$exit_code(set_color normal)
     end
-    echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
-    set_color normal
+    echo -e -n (_pure_prompt $exit_code) # print prompt
+    echo -e (_pure_prompt_ending) # reset colors and end prompt
+
+    set _pure_fresh_session false
 end
