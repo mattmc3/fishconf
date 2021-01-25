28060608b7957dbbf01cb2323d0eb8f0bca64cfd
mattmc3 <mattmc3@gmail.com>
2021-01-24 22:25:06 -0500

    Add neon-tetra prompt

--- conf.d/fisher.fish
+++ conf.d/fisher.fish
@@ -1,5 +1,3 @@
 # https://github.com/jorgebucaran/fisher#changing-the-installation-prefix
 set -g fisher_path ~/.config/fish/fisher
 plugin_load $fisher_path
-
-set pure_symbol_prompt (set_color red)❯(set_color yellow)❯(set_color green)❯(set_color normal)
