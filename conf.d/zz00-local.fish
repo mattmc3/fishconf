6d5c59e5408994b272aefdab78b19315233a6476
mattmc3 <mattmc3@gmail.com>
2021-02-03 10:13:40 -0500

    Change to dotfiles/local

--- conf.d/zz00-local.fish
+++ conf.d/zz00-local.fish
@@ -3,6 +3,6 @@ set -e fish_config_local
 if test -d $HOME/.config/fish/local
     plugin_load $HOME/.config/fish/local
 end
-if test -d $DOTFILES.local/fish
-    plugin_load $DOTFILES.local/fish
+if test -d $DOTFILES/local/fish
+    plugin_load $DOTFILES/local/fish
 end
