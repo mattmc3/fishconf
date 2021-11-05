3afd9ea8898a3118e9209df7a333b784edbdb707
mattmc3 <mattmc3@gmail.com>
2021-11-05 12:05:03 -0400

    Change prompt to scales (cust)

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,20 +1,15 @@
-# modify pure prompt to display fail code
-# https://github.com/pure-fish/pure/issues/188
-# https://github.com/pure-fish/pure/blob/master/functions/fish_prompt.fish
 function fish_prompt
-    set --local exit_code $status # save previous exit code
-
-    echo -e -n (_pure_prompt_beginning) # init prompt context (clear current line, etc.)
-    # add an additional newline if not compact prompt
-    test "$pure_enable_single_line_prompt" = true || echo
-    _pure_print_prompt_rows # manage default vs. compact prompt
-    _pure_place_iterm2_prompt_mark # place iTerm shell integration mark
-    # show the exit_code from the prior command
-    if test $exit_code -ne 0
-        echo -e -n (set_color red)$exit_code(set_color normal)
+    if test -n "$SSH_TTY"
+        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
     end
-    echo -e -n (_pure_prompt $exit_code) # print prompt
-    echo -e (_pure_prompt_ending) # reset colors and end prompt
 
-    set _pure_fresh_session false
+    echo -n (set_color blue)(prompt_pwd)' '
+
+    set_color -o
+    if fish_is_root_user
+        echo -n (set_color red)'# '
+    end
+    #echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+    echo -n (set_color cyan)'» '
+    set_color normal
 end
