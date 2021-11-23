4656dbd6f00eafe9cdde3e83422b35216aed976e
mattmc3 <mattmc3@gmail.com>
2021-11-22 23:06:00 -0500

    Remove pure prompt residuals

--- conf.d/prompt.fish
+++
@@ -1,8 +0,0 @@
-if test "$pure_prompt_initialized" != true
-    set -U pure_prompt_initialized true
-    set -U pure_symbol_prompt "»"
-    set -U pure_symbol_reverse_prompt "«"
-    set -U pure_color_success cyan
-    set -U pure_color_info magenta
-    set -U pure_color_git_dirty magenta
-end
