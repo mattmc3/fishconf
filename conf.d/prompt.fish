1ca5b35e6c72664f56439a0ead79692bf747d2c1
mattmc3 <mattmc3@gmail.com>
2021-11-26 12:42:24 -0500

    Use custom hydro prompt

--- conf.d/prompt.fish
+++ conf.d/prompt.fish
@@ -5,8 +5,10 @@ if test "$my_prompt_initialized" != true
     set -g hydro_symbol_git_ahead "⇡"
     set -g hydro_symbol_git_behind "⇣"
 
-    set hydro_color_git green
-    set hydro_color_prompt cyan
-    set hydro_color_pwd blue
-    set hydro_color_error red
+    set -g hydro_color_git green
+    set -g hydro_color_prompt cyan
+    set -g hydro_color_pwd blue
+    set -g hydro_color_error red
+    set -g hydro_multiline true
 end
+# plugin-load $PROJECTS/mattmc3/hydro
--- functions/plugins/plugin-load.fish
+++ functions/plugins/plugin-load.fish
@@ -1,19 +1,12 @@
-function plugin-load -a plugin -d "Load a plugin"
-    if not test -d $plugin
-        echo >&2 "Plugin directory argument missing or does not exist"
+function plugin-load -a plugin_dir -d "Load a plugin"
+    if not test -d "$plugin_dir"
+        echo >&2 "plugin-load: Plugin directory not found: '$plugin_dir'."
         return 1
     end
-    #set plugin (realpath $plugin)
-    if test -d $plugin/completions #; and not contains $plugin/completions $fish_complete_path
-        set fish_complete_path $fish_complete_path[1] $plugin/completions $fish_complete_path[2..-1]
-    end
-    if test -d $plugin/functions #; and not contains $plugin/functions $fish_function_path
-        set fish_function_path $fish_function_path[1] $plugin/functions $fish_function_path[2..-1]
-    end
-    if test -f $plugin/config.fish
-        builtin source "$plugin/config.fish"
-    end
-    for f in $plugin/conf.d/*.fish
-        builtin source "$f"
+    test -d $plugin_dir/completions; and set fish_complete_path $fish_complete_path[1] $plugin_dir/completions $fish_complete_path[2..-1]
+    test -d $plugin_dir/functions; and set fish_function_path $fish_function_path[1] $plugin_dir/functions $fish_function_path[2..-1]
+    test -f $plugin_dir/config.fish; and builtin source "$plugin_dir/config.fish"
+    for conffile in $plugin_dir/conf.d/*
+        builtin source "$conffile"
     end
 end
