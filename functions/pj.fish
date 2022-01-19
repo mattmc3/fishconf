70281eedfd9d2ef3ab99e1fc27fd8f09ae886616
mattmc3 <mattmc3@gmail.com>
2022-01-19 14:29:06 -0500

    Rename prj to pj

--- functions/pj.fish
+++ functions/pj.fish
@@ -1,4 +1,4 @@
-function prj --description "Project jump"
+function pj --description "Project jump"
     if ! command -v fzf >/dev/null
         echo >&2 "fzf command not found"
         return 1
@@ -6,10 +6,7 @@ function prj --description "Project jump"
 
     # determine the project home
     set -q PROJECTS || set PROJECTS ~/Projects
-    set prjlist
-    for d in $PROJECTS/**/.git/..
-        set -a prjlist (realpath $d)
-    end
+    set prjlist (realpath $PROJECTS/**/.git/..)
     set selection (printf "%s\n" $prjlist | sort | fzf --layout=reverse-list --query="$argv")
     if [ -d $PROJECTS/$selection ]
         cd $PROJECTS/$selection
