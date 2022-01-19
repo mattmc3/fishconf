e86e6045845ffc327ad63419b368b0ac17e09e9e
mattmc3 <mattmc3@gmail.com>
2022-01-19 12:56:17 -0500

    Fix faulty comments

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
-# config.fish isn't like zshrc/bashrc - not everything should be done here.
-# ~/.config/fish/conf.d runs first, so really this file is not even needed.
+# ~/.config/fish/conf.d runs first so my config is there
 
 # https://fishshell.com/docs/current/tutorial.html
 # https://github.com/jorgebucaran/fish-shell-cookbook
 # https://github.com/fish-shell/fish-shell/blob/master/share/config.fish
+# https://github.com/fish-shell/fish-shell/blob/da32b6c172dcfe54c9dc4f19e46f35680fc8a91a/share/config.fish#L257-L269
