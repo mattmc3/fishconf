19155430603c0653d208457542466a51989c051f
mattmc3 <mattmc3@gmail.com>
2022-01-19 12:57:33 -0500

    Change hydro init

--- conf.d/prompt.fish
+++ conf.d/prompt.fish
@@ -1,13 +1,11 @@
-if test "$hydro_prompt_initialized" != true
-    set -g hydro_prompt_initialized true
-    set -g hydro_symbol_prompt "»"
-    set -g hydro_symbol_git_dirty "*"
-    set -g hydro_symbol_git_ahead "⇡"
-    set -g hydro_symbol_git_behind "⇣"
+# set -q _hydro_initialized && exit || set -g _hydro_initialized true
+set -g hydro_symbol_prompt "»"
+set -g hydro_symbol_git_dirty "*"
+set -g hydro_symbol_git_ahead "⇡"
+set -g hydro_symbol_git_behind "⇣"
 
-    set -g hydro_color_git green
-    set -g hydro_color_prompt cyan
-    set -g hydro_color_pwd blue
-    set -g hydro_color_error red
-    set -g hydro_multiline true
-end
+set -g hydro_color_git green
+set -g hydro_color_prompt cyan
+set -g hydro_color_pwd blue
+set -g hydro_color_error red
+set -g hydro_multiline true
