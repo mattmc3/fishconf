f7caa0ff15b40fded9c21907de3ff425fa44f4e6
mattmc3 <mattmc3@gmail.com>
2022-01-31 22:45:33 -0500

    Let Fisher manage dotfiles.local plugin

--- conf.d/zz00-local.fish
+++
@@ -1,7 +0,0 @@
-# local settings
-if ! test -d $__fish_config_dir/local && test -d $DOTFILES.local/fish
-    ln -s $DOTFILES.local/fish $__fish_config_dir/local
-end
-if test -d $__fish_config_dir/local
-    plugin-load $__fish_config_dir/local
-end
