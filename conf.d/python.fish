d968a831d28a54204f83edc4d7f935a4327e0af6
mattmc3 <mattmc3@gmail.com>
2022-01-31 22:45:04 -0500

    Consolidate apps conf

--- conf.d/python.fish
+++
@@ -1,8 +0,0 @@
-if not set -q VENV_HOME
-    if set -q XDG_DATA_HOME
-        set -gx VENV_HOME $XDG_DATA_HOME/venvs
-    else
-        set -gx VENV_HOME ~/.local/share/venvs
-    end
-end
-test -d $VENV_HOME; or mkdir -p "$VENV_HOME"
