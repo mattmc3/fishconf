17baf67753742c380e2c27e75a0f4803ba0774cd
mattmc3 <mattmc3@gmail.com>
2022-02-04 21:53:48 -0500

    Make pretty

--- functions/macos/bright.fish
+++ functions/macos/bright.fish
@@ -1,8 +1,8 @@
 function bright -a direction -d 'Make the screen bright'
     switch "$direction"
-    case d down dim less
-        osascript -e 'tell application "System Events"' -e 'key code 145' -e ' end tell'
-    case u up more ""
-        osascript -e 'tell application "System Events"' -e 'key code 144' -e ' end tell'
+        case d down dim less
+            osascript -e 'tell application "System Events"' -e 'key code 145' -e ' end tell'
+        case u up more ""
+            osascript -e 'tell application "System Events"' -e 'key code 144' -e ' end tell'
     end
 end
--- functions/up.fish
+++ functions/up.fish
@@ -1,5 +1,5 @@
 function up -d 'Travel up any number of directories'
-    test -n "$argv" || set argv "1"
+    test -n "$argv" || set argv 1
     set -l balloons (string repeat -n "$argv[1]" "../" 2>/dev/null) || begin
         echo "Invalid arguments '$argv'"\n"Usage: "(status function)" <levels>" >&2
         return 2
