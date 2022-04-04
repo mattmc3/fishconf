36c8597bfed00f768cfc93777f5d4e1e7950ca02
mattmc3 <mattmc3@gmail.com>
2022-04-04 16:43:33 -0400

    Add fbench

---
+++ functions/fbench.fish
@@ -0,0 +1,12 @@
+function fbench \
+    --description 'benchmark fish' \
+    --argument-names shellname
+
+    test -n "$shellname" || set shellname fish
+    echo "running fish 10 times..."
+    for i in (seq 10)
+        /usr/bin/time fish -i -c exit
+    end
+    echo "Writing fish profile file to $__fish_config_dir/fishprof.txt"
+    fish --profile-startup=$__fish_config_dir/fishprof.txt -c "exit"
+end
