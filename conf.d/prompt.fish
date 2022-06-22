1e44808854f80d32e867361e7e02c13563853277
mattmc3 <mattmc3@gmail.com>
2022-06-22 10:15:49 -0400

    Update prompt for new hydro

--- conf.d/prompt.fish
+++ conf.d/prompt.fish
@@ -3,10 +3,11 @@ set -g hydro_symbol_prompt "»"
 set -g hydro_symbol_git_dirty "*"
 set -g hydro_symbol_git_ahead "⇡"
 set -g hydro_symbol_git_behind "⇣"
+set -g hydro_pre_prompt "\n"
 
 set -g hydro_color_git green
 set -g hydro_color_prompt cyan
 set -g hydro_color_pwd blue
 set -g hydro_color_error red
 set -g hydro_multiline true
-set -g hydro_spacious true
+set -g fish_prompt_pwd_dir_length -1
