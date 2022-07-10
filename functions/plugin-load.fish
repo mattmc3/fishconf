e62a39c27294ffac3dfb4078eed7367a22c46485
mattmc3 <mattmc3@gmail.com>
2022-07-09 23:57:09 -0400

    Only load .fish files

--- functions/plugin-load.fish
+++ functions/plugin-load.fish
@@ -6,7 +6,7 @@ function plugin-load -a plugin_dir -d "Load a plugin"
     test -d $plugin_dir/completions; and set fish_complete_path $fish_complete_path[1] $plugin_dir/completions $fish_complete_path[2..-1]
     test -d $plugin_dir/functions; and set fish_function_path $fish_function_path[1] $plugin_dir/functions $fish_function_path[2..-1]
     test -f $plugin_dir/config.fish; and builtin source "$plugin_dir/config.fish"
-    for conffile in $plugin_dir/conf.d/*
+    for conffile in $plugin_dir/conf.d/*.fish
         builtin source "$conffile"
     end
 end
