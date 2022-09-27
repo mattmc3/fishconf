eaa74c9595469e639d2c4e1dee7bba93d27a80dc
mattmc3 <mattmc3@gmail.com>
2022-09-27 08:24:08 -0400

    Change dotf for non-bare repo

--- functions/dotf.fish
+++ functions/dotf.fish
@@ -1,5 +1,7 @@
 function dotf \
     --description 'dotfiles bare git repo'
 
-    git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $argv
+    set -q DOTFILES || set -U DOTFILES $HOME/.config/dotfiles
+    cd $DOTFILES
+    #git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $argv
 end
