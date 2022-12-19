187a53e3e12d34e613ec13787098da28cd0081d8
mattmc3 <mattmc3@gmail.com>
2022-12-19 11:13:54 -0500

    Change grep alias

--- functions/grep.fish
+++ functions/grep.fish
@@ -1,3 +1,3 @@
-function grep --wraps='command grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}' --description 'alias grep command grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}'
-    command grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} $argv
+function grep --wraps='command grep --color=auto --exclude-dir={.git,.hg,.svn}' --description 'alias grep command grep --color=auto --exclude-dir={.git,.hg,.svn}'
+    command grep --color=auto --exclude-dir={.git,.hg,.svn} $argv
 end
