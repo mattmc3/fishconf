7b582e4d21fd7b09ec202ec11ea23f2491db53e5
mattmc3 <mattmc3@gmail.com>
2023-01-08 22:51:43 -0500

    Change fisher init

--- conf.d/01-plugins.fish
+++ conf.d/01-plugins.fish
@@ -1,1 +1,24 @@
-fisher_init
+set -q fisher_path || set -U fisher_path $__fish_config_dir/.fisher
+set -q my_plugins_path || set -U my_plugins_path $__fish_config_dir/plugins
+
+if test "$fisher_paths_initialized" != true
+    set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..]
+    set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..]
+    set -g fisher_paths_initialized true
+end
+
+if not test -d $fisher_path
+    functions -e fisher &>/dev/null
+    mkdir -p $fisher_path
+
+    curl -sL https://git.io/fisher | source
+    fisher install jorgebucaran/fisher
+    fisher update
+end
+
+for file in $fisher_path/conf.d/*.fish
+    if ! test -f $__fish_config_dir/conf.d/(path basename -- $file)
+        and test -f $file && test -r $file
+        builtin source $file
+    end
+end
