ffa101e7b389970f00b71e58a89ed5e64efdb568
mattmc3 <mattmc3@gmail.com>
2023-01-18 20:30:20 -0500

    Fallback propmpt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,3 +1,12 @@
 function fish_prompt --description Hydro
-    echo -e "\n$_hydro_color_pwd$_hydro_pwd\x1b[0m $_hydro_color_git$$_hydro_git\x1b[0m$_hydro_color_duration$_hydro_cmd_duration\x1b[0m$$_hydro_addons$_hydro_status\x1b[0m "
+    if set -q _hydro_pwd
+        echo -e "\n$_hydro_color_pwd$_hydro_pwd\x1b[0m $_hydro_color_git$$_hydro_git\x1b[0m$_hydro_color_duration$_hydro_cmd_duration\x1b[0m$$_hydro_addons$_hydro_status\x1b[0m "
+    else
+        set -l default_prompt_files \
+            /opt/homebrew/Cellar/fish/*/share/fish/functions/fish_prompt.fish \
+            /usr/local/Cellar/fish/*/share/fish/functions/fish_prompt.fish
+        for prompt in $default_prompt_files
+            test -f $prompt && source $prompt && return
+        end
+    end
 end
