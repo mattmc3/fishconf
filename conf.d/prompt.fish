866ef4758cf427eeb330e81fe7177de0cd73f647
mattmc3 <mattmc3@gmail.com>
2023-03-03 10:45:13 -0500

    Switch from hydro to starship

--- conf.d/prompt.fish
+++ conf.d/prompt.fish
@@ -1,13 +1,16 @@
 # set -q _hydro_initialized && exit || set -g _hydro_initialized true
-set -g hydro_symbol_prompt "»"
-set -g hydro_symbol_git_dirty "*"
-set -g hydro_symbol_git_ahead "⇡"
-set -g hydro_symbol_git_behind "⇣"
-set -g hydro_pre_prompt "\n"
+# set -g hydro_symbol_prompt "»"
+# set -g hydro_symbol_git_dirty "*"
+# set -g hydro_symbol_git_ahead "⇡"
+# set -g hydro_symbol_git_behind "⇣"
+# set -g hydro_pre_prompt "\n"
 
-set -g hydro_color_git green
-set -g hydro_color_prompt cyan
-set -g hydro_color_pwd blue
-set -g hydro_color_error red
-set -g hydro_multiline true
-set -g fish_prompt_pwd_dir_length -1
+# set -g hydro_color_git green
+# set -g hydro_color_prompt cyan
+# set -g hydro_color_pwd blue
+# set -g hydro_color_error red
+# set -g hydro_multiline true
+# set -g fish_prompt_pwd_dir_length -1
+
+set STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/fish.toml
+starship init fish | source
