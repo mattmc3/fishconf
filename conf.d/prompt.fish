d13250370678c8ac381378be7b99a0ddb8c5104f
mattmc3 <mattmc3@gmail.com>
2023-04-28 15:55:29 -0400

    Prompt configs

--- conf.d/prompt.fish
+++ conf.d/prompt.fish
@@ -1,19 +1,21 @@
-set -gx STARSHIP_CONFIG $__fish_config_dir/themes/prompt/zebrafish.toml
-starship init fish | source
+set -q FISH_PROMPT_THEME || set -U FISH_PROMPT_THEME hydro
 
-# set -q _hydro_initialized && exit || set -g _hydro_initialized true
-# set -g hydro_symbol_prompt "»"
-# set -g hydro_symbol_git_dirty "*"
-# set -g hydro_symbol_git_ahead "⇡"
-# set -g hydro_symbol_git_behind "⇣"
-# # set -g hydro_pre_prompt "\n"
+switch $FISH_PROMPT_THEME
+case hydro
+    set -g hydro_symbol_prompt "❱"
+    set -g hydro_symbol_git_dirty "*"
+    set -g hydro_symbol_git_ahead "⇡"
+    set -g hydro_symbol_git_behind "⇣"
+    # set -g hydro_pre_prompt "\n"
 
-# set -g hydro_color_git green
-# set -g hydro_color_prompt cyan
-# set -g hydro_color_pwd blue
-# set -g hydro_color_error red
-# # set -g hydro_multiline true
-# set -g fish_prompt_pwd_dir_length 1
+    set -g hydro_color_git magenta
+    set -g hydro_color_prompt blue
+    set -g hydro_color_pwd green
+    set -g hydro_color_error red
+    # set -g hydro_multiline true
+    set -g fish_prompt_pwd_dir_length 1
 
-# set -gx STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/fish.toml
-# starship init fish | source
+case starship
+    set -gx STARSHIP_CONFIG $__fish_config_dir/themes/prompt/zebrafish.toml
+    starship init fish | source
+end
