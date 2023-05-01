2d998cc7ba6aabdf9744524b033b8e3dc9a6cab5
mattmc3 <mattmc3@gmail.com>
2023-05-01 17:56:29 -0400

    Move cd-ls to local plugins

--- plugins/cd-ls/conf.d/cd-ls.fish
+++ plugins/cd-ls/conf.d/cd-ls.fish
@@ -1,4 +1,4 @@
-set -q ls_after_cd || set -xg ls_after_cd true
+set -q ls_after_cd || set -g ls_after_cd true
 
 # show directory listing on directory change
 function __ls_after_cd__on_variable_pwd --on-variable PWD
