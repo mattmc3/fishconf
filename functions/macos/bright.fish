84c8c19ffc4169438203ac49f6487c2992a8e31c
mattmc3 <mattmc3@gmail.com>
2023-05-01 17:46:20 -0400

    Clean up macos plugins

--- functions/macos/bright.fish
+++
@@ -1,8 +0,0 @@
-function bright -a direction -d 'Make the screen bright'
-    switch "$direction"
-        case d down dim less
-            osascript -e 'tell application "System Events"' -e 'key code 145' -e ' end tell'
-        case u up more ""
-            osascript -e 'tell application "System Events"' -e 'key code 144' -e ' end tell'
-    end
-end
