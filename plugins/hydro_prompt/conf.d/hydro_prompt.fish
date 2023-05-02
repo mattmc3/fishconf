8828d2c619f8095902ed191d31723ba6c89dd79b
mattmc3 <mattmc3@gmail.com>
2023-05-02 19:31:37 -0400

    Update hydro prompt vars

--- plugins/hydro_prompt/conf.d/hydro_prompt.fish
+++ plugins/hydro_prompt/conf.d/hydro_prompt.fish
@@ -1,13 +1,14 @@
-set -q hydro_symbol_prompt || set -g hydro_symbol_prompt "❱"
-set -q hydro_symbol_git_dirt || set -g hydro_symbol_git_dirty "*"
-set -q hydro_symbol_git_ahead || set -g hydro_symbol_git_ahead "⇡"
-set -q hydro_symbol_git_behind || set -g hydro_symbol_git_behind "⇣"
-# set -q hydro_pre_prompt || set -g hydro_pre_prompt "\n"
+# hydro prompt vars
+set -g hydro_symbol_prompt "❱"
+set -g hydro_symbol_git_dirty "*"
+set -g hydro_symbol_git_ahead "⇡"
+set -g hydro_symbol_git_behind "⇣"
+# set -g hydro_pre_prompt "\n"
 
-set -q hydro_color_git || set -g hydro_color_git brcyan
-set -q hydro_color_prompt || set -g hydro_color_prompt blue
-set -q hydro_color_pwd || set -g hydro_color_pwd green
-set -q hydro_color_error || set -g hydro_color_error red
-set -q hydro_color_duration || set -g hydro_color_duration yellow
-# set -q hydro_multiline || set -g hydro_multiline true
-set -q fish_prompt_pwd_dir_length || set -g fish_prompt_pwd_dir_length 1
+set -g hydro_color_git brcyan
+set -g hydro_color_prompt blue
+set -g hydro_color_pwd green
+set -g hydro_color_error red
+set -g hydro_color_duration yellow
+# set -g hydro_multiline true
+set -g fish_prompt_pwd_dir_length 1
