b409ac5aa1cac0e9c4afc1ae55f193e3159e6d8e
mattmc3 <mattmc3@gmail.com>
2023-05-02 14:12:46 -0400

    Move time to the right prompt

--- plugins/hydro_prompt/functions/fish_right_prompt.fish
+++ plugins/hydro_prompt/functions/fish_right_prompt.fish
@@ -1,3 +1,3 @@
 function fish_right_prompt --description Hydro
-    echo -e "$_hydro_color_git$$_hydro_git$hydro_color_normal"
+    echo -e "$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_color_git$$_hydro_git$hydro_color_normal"
 end
