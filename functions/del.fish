ca256bf8c556031b91cfb522eeb46217bce216e2
mattmc3 <mattmc3@gmail.com>
2023-05-06 22:18:53 -0400

    Use rem

--- functions/del.fish
+++ functions/del.fish
@@ -1,5 +1,8 @@
 function del --description "safe rm"
-    if type -q shell-safe-rm
+    if type -q rem
+        touch -- $argv
+        rem $argv
+    else if type -q shell-safe-rm
         shell-safe-rm $argv
     else
         echo "shell-safe-rm not found: https://github.com/kaelzhang/shell-safe-rm" >&2
