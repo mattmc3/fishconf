33e12ba409d2bfd014279ef11a22a52f15562291
mattmc3 <mattmc3@gmail.com>
2023-05-06 22:21:36 -0400

    Change hydro colors

--- plugins/hydro_prompt/conf.d/hydro_prompt.fish
+++ plugins/hydro_prompt/conf.d/hydro_prompt.fish
@@ -5,9 +5,9 @@ set -g hydro_symbol_git_ahead "⇡"
 set -g hydro_symbol_git_behind "⇣"
 # set -g hydro_pre_prompt "\n"
 
-set -g hydro_color_git brcyan
-set -g hydro_color_prompt blue
-set -g hydro_color_pwd green
+set -g hydro_color_git brmagenta
+set -g hydro_color_prompt green
+set -g hydro_color_pwd blue
 set -g hydro_color_error red
 set -g hydro_color_duration yellow
 # set -g hydro_multiline true
