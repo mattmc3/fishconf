8b84deef5ac251698ecc20c3fe09f5939f97d6a4
mattmc3 <mattmc3@gmail.com>
2023-06-28 15:28:25 -0400

    Update environment vars

--- conf.d/00-uvars.fish
+++ conf.d/00-uvars.fish
@@ -1,5 +1,5 @@
 #
-# uvars - set fish universals
+# uvars - set fish universals (sparingly)
 #
 
 set -q MY_UVARS_SET
@@ -22,54 +22,5 @@ for xdg_dir in \
 
     [ -d $xdg_dir ]; or mkdir -p $xdg_dir
 end
-
-# XDG apps
-set -Ux SQLITE_HISTORY $XDG_DATA_HOME/sqlite_history
-set -Ux LESSHISTFILE $XDG_DATA_HOME/lesshst
-set -Ux GNUPGHOME $XDG_DATA_HOME/gnupg
-
-# Common shell variables
-switch (uname -s)
-    case Darwin
-        set -Ux BROWSER open
-end
-
-set -Ux PAGER less
-set -Ux VISUAL code
-set -Ux EDITOR vim
-
-# My special dirs
-set -Ux MY_PROJECTS $HOME/Projects
-set -U my_plugins_path $__fish_config_dir/plugins
-
-# Man pages paths
-if test (count $MANPATH) -eq 0
-    set -U MANPATH /usr/local/share/man
-else
-    set -U MANPATH /usr/local/share/man $MANPATH
-end
-
-# We always want /usr/local/bin dirs, but on Apple Silicon we also need
-# /opt/homebrew/bin dirs
-fish_add_path /usr/local/bin /usr/local/sbin
-if [ -e /opt/homebrew/bin/brew ]
-    set -U HOMEBREW_PREFIX /opt/homebrew
-    fish_add_path $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
-    set -U MANPATH $HOMEBREW_PREFIX/share/man $MANPATH
-else if [ -e /usr/local/bin/brew ]
-    set -U HOMEBREW_PREFIX /usr/local
-end
-
-# Other homebrew vars
-set -Ux HOMEBREW_NO_ANALYTICS 1
-
-# theme
-set -Ux MY_THEME tokyonight_night
-
-# Reel plugins
-set -Ux REEL_HOME $XDG_DATA_HOME/reel
-
-# Add my bin directory too.
-fish_add_path ~/bin
 
 set -U MY_UVARS_SET true
--- conf.d/01-env.fish
+++ conf.d/01-env.fish
@@ -5,6 +5,17 @@
 # disable new user greeting
 set fish_greeting
 
+# Editor variables
+set -gx PAGER less
+set -gx VISUAL code
+set -gx EDITOR vim
+
+# Browser
+switch (uname -s)
+case Darwin
+    set -gx BROWSER open
+end
+
 # dotfiles
 set -q DOTFILES || set -gx DOTFILES ~/.config/dotfiles
 set -q ZDOTDIR || set -gx ZDOTDIR ~/.config/zsh
@@ -12,8 +23,23 @@ set -q ZDOTDIR || set -gx ZDOTDIR ~/.config/zsh
 # initial working directory
 set -g IWD $PWD
 
+# Add my bin directory to path.
+fish_add_path ~/bin
+
 # add function subdirs to fish_function_path
 set fish_function_path (path resolve $__fish_config_dir/functions/*/) $fish_function_path
 
 # add completion subdirs to fish_completion_path
 set fish_complete_path (path resolve $__fish_config_dir/completions/*/) $fish_complete_path
+
+# XDG apps
+set -gx SQLITE_HISTORY $XDG_DATA_HOME/sqlite_history
+set -gx LESSHISTFILE $XDG_DATA_HOME/lesshst
+set -gx GNUPGHOME $XDG_DATA_HOME/gnupg
+
+# My special dirs
+set -gx MY_PROJECTS $HOME/Projects
+set -gx my_plugins_path $__fish_config_dir/plugins
+
+# Reel plugins
+set -gx REEL_HOME $XDG_DATA_HOME/reel
