74e47656a5768295a2baca7f160cfc938fbc41af
mattmc3 <mattmc3@gmail.com>
2023-06-28 15:59:57 -0400

    Stop using rm

---
+++ functions/del.fish
@@ -0,0 +1,11 @@
+function del --description "safe rm"
+    if functions -q trash
+        argparse --name=del 'r' 'f' 'i' -- $argv; or return 1
+        trash $argv
+    else if type -q shell-safe-rm
+        shell-safe-rm $argv
+    else
+        echo "shell-safe-rm not found: https://github.com/kaelzhang/shell-safe-rm" >&2
+        commandline -r "/bin/rm $argv"
+    end
+end
--- functions/trash.fish
+++ functions/trash.fish
@@ -1,19 +1,29 @@
 function trash -d "Move a specified file to the Trash"
     if test (count $argv) -eq 0
-        echo "Usage: trash <list>" >&2
+        echo >&2 "Usage: trash <files>"
         return 1
     end
 
-    set -l trash_dir "$HOME/.Trash"
-    for item in $argv
-        if test -e $item
-            set -l item_name (basename $item)
-            if test -e "$trash_dir/$item_name"
-                set -l current_time (date "+%s")
-                mv -f "$item" "$trash_dir/$item_name $current_time"
-            else
-                mv -f "$item" "$trash_dir/"
-            end
+    if test (uname -s) != "Darwin"
+        echo >&2 "trash: macOS not detected."
+        return 1
+    end
+
+    set -l file
+    set -l files
+    for file in $argv
+        if test -e $file
+            set -a files "the POSIX file \""(path resolve $file)"\""
+        else
+            echo >&2 "trash: No such file or directory '$file'."
+            return 1
         end
     end
+
+    if test (count $files) -eq 0; then
+        echo >&2 'usage: trash <files...>'
+        return 64  # match rm's return code
+    end
+
+    osascript 2>&1 >/dev/null -e "tell app \"Finder\" to move { "(string join ", " $files)" } to trash"
 end
