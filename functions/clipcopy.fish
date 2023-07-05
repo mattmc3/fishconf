e6371294d07ea4d6feddc6363ad91097df6b1e18
mattmc3 <mattmc3@gmail.com>
2023-07-05 19:16:18 -0400

    Add clipcopy/clippaste

---
+++ functions/clipcopy.fish
@@ -0,0 +1,12 @@
+function clipcopy --description 'Copy data to clipboard'
+    set -l ostype (uname -s)
+    if test "$ostype" = "Darwin"
+        if test (count $argv) -eq 0
+            cat /dev/stdin | pbcopy
+        else
+            cat $argv | pbcopy
+        end
+    else
+        echo >&2 "Unsupported OS '$ostype'."
+    end
+end
---
+++ functions/clippaste.fish
@@ -0,0 +1,8 @@
+function clippaste --description 'Paste data from clipboard'
+    set -l ostype (uname -s)
+    if test $ostype = "Darwin"
+        pbpaste
+    else
+        echo >&2 "Unsupported OS: '$ostype'."
+    end
+end
