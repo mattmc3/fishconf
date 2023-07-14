7ed878e512148d6a3401f32021168f318c98af01
mattmc3 <mattmc3@gmail.com>
2023-07-14 10:46:41 -0400

    Use starship prompt

---
+++ conf.d/prompt.fish
@@ -0,0 +1,19 @@
+# hydro
+# # hydro prompt vars
+# set -g hydro_symbol_prompt "❱"
+# #set -g hydro_symbol_git_dirty "*"
+# set -g hydro_symbol_git_ahead "⇡"
+# set -g hydro_symbol_git_behind "⇣"
+# # set -g hydro_pre_prompt "\n"
+
+# set -g hydro_color_git brmagenta
+# set -g hydro_color_prompt green
+# set -g hydro_color_pwd cyan
+# set -g hydro_color_error red
+# set -g hydro_color_duration yellow
+# # set -g hydro_multiline true
+# set -g fish_prompt_pwd_dir_length 1
+
+# starship
+set -gx STARSHIP_CONFIG $__fish_config_dir/themes/starship.toml
+source (starship init fish --print-full-init | psub)
--- functions/fish_right_prompt.fish
+++
@@ -1,3 +0,0 @@
-function fish_right_prompt --description Hydro
-    echo -e "$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_color_git$$_hydro_git$hydro_color_normal"
-end
