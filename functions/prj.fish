b5680bab2b7b4fdb913efd23712d3c3919653717
mattmc3 <mattmc3@gmail.com>
2023-07-18 09:32:13 -0400

    Update prj

--- functions/prj.fish
+++ functions/prj.fish
@@ -1,19 +1,18 @@
-function prj --description "Project jump"
-    if ! command -v fzf >/dev/null
-        echo >&2 "fzf command not found"
+function prj --description "Project jumper"
+    if not command -q fzf
+        echo >&2 "prj: fzf command not found. Install with your OS package manager."
         return 1
     end
 
     # determine the project home
-    set -q MY_PROJECTS || set MY_PROJECTS ~/Projects
-    set gitprj $MY_PROJECTS/**/.git/
+    set -q MY_PROJECTS || set -l MY_PROJECTS ~/Projects
+    set -l gitprj $MY_PROJECTS/**/.git/
     set gitprj (path resolve $gitprj/..)
 
-    set prjlist (string replace $MY_PROJECTS/ "" $gitprj)
-    set selection (printf '%s\n' $prjlist | sort | fzf --layout=reverse-list --query="$argv")
-    if test -d $MY_PROJECTS/$selection
-        cd $MY_PROJECTS/$selection
-    else
-        echo $MY_PROJECTS/$selection
-    end
+    # use fzf to navigate to a project
+    set -l prjlist (string replace $MY_PROJECTS/ "" $gitprj)
+    set -l selection (printf '%s\n' $prjlist | sort | fzf --layout=reverse-list --query="$argv")
+    test $status -eq 0 || return $status
+    echo "Navigating to '$selection'."
+    cd $MY_PROJECTS/$selection
 end
