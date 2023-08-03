f22f01fd3fb34d14ac7d8652415cd052c94c76de
mattmc3 <mattmc3@gmail.com>
2023-08-02 20:57:27 -0400

    Update repo/reel

--- functions/reel.fish
+++ functions/reel.fish
@@ -1,16 +1,28 @@
-set -q REEL_HOME || set -gx REEL_HOME $XDG_DATA_HOME/reel
+# Reel - a simple Fish plugin manager
+
+# Setup Reel
+set -q reel_path || set -gx reel_path $__fish_config_dir/.reel
 
 function __reel_update -a repo
-    set -l repodir $REEL_HOME/$repo
-    set -l oldsha (command git -C $repodir rev-parse --short HEAD)
-    command git -C $repodir pull --quiet --ff --depth 1 --rebase --autostash
-    set -l newsha (command git -C $repodir rev-parse --short HEAD)
+    set -l plugindir $reel_path/plugins/(path basename $repo)
+    set -l oldsha (command git -C $plugindir rev-parse --short HEAD)
+    command git -C $plugindir pull --quiet --ff --depth 1 --rebase --autostash
+    set -l newsha (command git -C $plugindir rev-parse --short HEAD)
     if test $oldsha != $newsha
-        echo "Repo updated: "(path basename $repodir)" ($oldsha->$newsha)"
+        echo "Repo updated: "(path basename $plugindir)" ($oldsha->$newsha)"
     end
 end
 
-function reel -a cmd --description "Deal with repos"
+function __reel_plugin_repo -a plugindir
+    set -l url_parts (
+        command git -C $plugindir config remote.origin.url |
+        string replace ':' '/' |
+        string split '/'
+    )
+    echo $url_parts[-2]"/"$url_parts[-1]
+end
+
+function reel -a cmd --description "A simple Fish plugin manager"
     # reel main function
     set argv $argv[2..-1]
 
@@ -26,10 +38,10 @@ function reel -a cmd --description "Deal with repos"
 
     switch "$cmd"
         case home
-            echo $REEL_HOME
+            echo $reel_path
         case ls
-            for repodir in (path dirname $REEL_HOME/*/*/.git)
-                echo (path basename (path dirname $repodir))"/"(path basename $repodir)
+            for plugindir in (path dirname $reel_path/plugins/*/.git)
+                __reel_plugin_repo $plugindir
             end
         case up
             echo "Checking for updates..."
@@ -42,11 +54,11 @@ function reel -a cmd --description "Deal with repos"
         case rm
             set err 0
             for repo in $argv
-                if not string match --quiet "$HOME/*" "$REEL_HOME"
-                    echo "Reel home not set correctly '$REEL_HOME'." >&2
+                if not string match --quiet "$HOME/*" "$reel_path"
+                    echo >&2 "reel: 'reel_path' not set correctly '$reel_path'."
                     return 1
-                else if [ -d $REEL_HOME/$repo ]
-                    command rm -rf -- $REEL_HOME/$repo
+                else if [ -d $reel_path/plugins/(path basename $repo) ]
+                    command rm -rf -- $reel_path/plugins/(path basename $repo)
                 else
                     echo "Repo not found '$repo'."
                     set err 1
@@ -56,7 +68,7 @@ function reel -a cmd --description "Deal with repos"
         case in
             set err 1
             for repo in $argv
-                set -l plugindir $REEL_HOME/$repo
+                set -l plugindir $reel_path/plugins/(path basename $repo)
                 if not test -d $plugindir
                     echo "Cloning $repo..."
                     command git clone --quiet --depth 1 --recursive --shallow-submodules \
@@ -66,6 +78,19 @@ function reel -a cmd --description "Deal with repos"
             end
             wait
             return $err
+        case init
+            if test "$reel_paths_initialized" != true
+                set fish_function_path $fish_function_path[1] $reel_path/functions $fish_function_path[2..]
+                set fish_complete_path $fish_complete_path[1] $reel_path/completions $fish_complete_path[2..]
+                set -g reel_paths_initialized true
+            end
+
+            for file in $reel_path/conf.d/*.fish
+                if ! test -f $__fish_config_dir/conf.d/(path basename -- $file)
+                    and test -f $file && test -r $file
+                    builtin source $file
+                end
+            end
         case \*
             echo >&2 "reel: unknown command '"$cmd"'"
             return 1
---
+++ functions/repo.fish
@@ -0,0 +1,80 @@
+if not set -q REPO_HOME
+    if set -q XDG_CACHE_HOME
+        set -gx REPO_HOME $XDG_CACHE_HOME/repos
+    else
+        set -gx REPO_HOME $HOME/.cache/repos
+    end
+end
+
+function __repo_update -a repo
+    set -l repodir $REPO_HOME/$repo
+    set -l oldsha (command git -C $repodir rev-parse --short HEAD)
+    command git -C $repodir pull --quiet --ff --depth 1 --rebase --autostash
+    set -l newsha (command git -C $repodir rev-parse --short HEAD)
+    if test $oldsha != $newsha
+        echo "$repo updated: "(path basename $repodir)" ($oldsha->$newsha)"
+    else
+        echo "$repo... no updates."
+    end
+end
+
+function repo -a cmd --description "Deal with git repos"
+    # repo main function
+    set argv $argv[2..-1]
+
+    # piped/redirected input
+    if not test -t 0
+        while read -l line
+            set line (string replace -r '#.*$' "" $line | string trim)
+            if [ $line != "" ]
+                set argv $argv $line
+            end
+        end
+    end
+
+    switch "$cmd"
+        case home
+            echo $REPO_HOME
+        case ls
+            for repodir in (path dirname $REPO_HOME/*/*/.git)
+                echo (path basename (path dirname $repodir))"/"(path basename $repodir)
+            end
+        case up update
+            echo "Checking for updates..."
+            for repo in (repo ls)
+                __repo_update $repo
+            end
+            wait
+            echo "Update complete."
+        case rm
+            set err 0
+            for repo in $argv
+                if not string match --quiet "$HOME/*" "$REPO_HOME"
+                    echo "repo home not set correctly '$REPO_HOME'." >&2
+                    return 1
+                else if [ -d $REPO_HOME/$repo ]
+                    command rm -rf -- $REPO_HOME/$repo
+                else
+                    echo "Repo not found '$repo'."
+                    set err 1
+                end
+            end
+            return $err
+        case in
+            set err 1
+            for repo in $argv
+                set -l plugindir $REPO_HOME/$repo
+                if not test -d $plugindir
+                    echo "Cloning $repo..."
+                    command git clone --quiet --depth 1 --recursive --shallow-submodules \
+                        https://github.com/$repo $plugindir &
+                    set err 0
+                end
+            end
+            wait
+            return $err
+        case \*
+            echo >&2 "repo: unknown command '"$cmd"'"
+            return 1
+    end
+end
