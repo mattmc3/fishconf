f293fe44ade363ac2cb34051f2b2c23157ffe808
mattmc3 <mattmc3@gmail.com>
2024-01-11 12:44:49 -0500

    Use TMUXHOME

--- functions/alias/tmux.fish
+++ functions/alias/tmux.fish
@@ -1,3 +1,6 @@
-function tmux --wraps='tmux' --description 'alias tmux command tmux -f "$XDG_CONFIG_HOME"/tmux/tmux.conf'
-    command tmux -f "$XDG_CONFIG_HOME"/tmux/tmux.conf $argv
+function tmux --wraps='tmux' --description 'alias tmux command tmux -f $TMUXHOME/tmux.conf'
+    set -q TMUXHOME; or set -gx TMUXHOME $XDG_CONFIG_HOME/tmux
+    test -d $TMUXHOME; or mkdir -p $TMUXHOME
+    test -e $TMUXHOME/tmux.conf; or touch $TMUXHOME/tmux.conf
+    command tmux -f $TMUXHOME/tmux.conf $argv
 end
