4a7fb80f53e9d7ca00213d75fddf270aeec54886
mattmc3 <mattmc3@gmail.com>
2024-03-19 20:56:22 -0400

    Update cachecmd

--- functions/cachecmd.fish
+++ functions/cachecmd.fish
@@ -1,9 +1,5 @@
 function cachecmd --description "Cache a command"
-    # Ensure cache dir exists
-    set -q XDG_CACHE_HOME; or set -Ux XDG_CACHE_HOME $HOME/.cache
-    set -q __fish_cache_dir; or set -Ux __fish_cache_dir $XDG_CACHE_HOME/fish
-    test -d $__fish_cache_dir; or mkdir -p $__fish_cache_dir
-
+    set_xdg_basedirs
     set --local cmdname (
         string join '-' $argv |
         string replace -a '/' '_' |
@@ -11,7 +7,7 @@ function cachecmd --description "Cache a command"
     )
     set --local cachefile $__fish_cache_dir/$cmdname.fish
     if not test -f $cachefile
-        $argv > $cachefile
+        $argv >$cachefile
     end
     cat $cachefile
 end
