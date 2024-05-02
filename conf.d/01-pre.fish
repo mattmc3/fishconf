9b8d44b108dfebf2b728388dca2b073d5e2b613b
mattmc3 <mattmc3@gmail.com>
2024-05-02 10:07:42 -0400

    Update pre

--- conf.d/01-pre.fish
+++ conf.d/01-pre.fish
@@ -2,19 +2,6 @@
 # pre - First things first.
 #
 
-# Run 'brew shellenv | source' in order of preference.
-for brew_prefix in \
-    $HOME/.homebrew \
-    /opt/homebrew \
-    /usr/local
-
-    if test -e "$brew_prefix/bin/brew"
-        cachecmd "$brew_prefix/bin/brew" shellenv | source
-        break
-    end
-end
-set -e brew_prefix
-
 # Add keg-only apps
 for app in ruby curl sqlite
     if test -d "$HOMEBREW_PREFIX/opt/$app/bin"
@@ -42,4 +29,5 @@ set fish_complete_path (path resolve $__fish_config_dir/completions/*/) $fish_co
 
 # Add bin directories to path.
 fish_add_path --prepend $HOME/.local/bin
+fish_add_path --prepend $HOME/go/bin
 fish_add_path --prepend $HOME/bin
