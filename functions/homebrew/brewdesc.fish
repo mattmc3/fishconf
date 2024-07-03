156c9c76f27758812ba9028683a82ed04d31097f
mattmc3 <mattmc3@gmail.com>
2024-07-03 16:48:27 -0400

    Use my homebrew plugin

--- functions/homebrew/brewdesc.fish
+++
@@ -1,5 +0,0 @@
-function brewdesc -d 'Show descriptions of brew installs'
-    brew leaves |
-        xargs brew desc --eval-all |
-        string replace -r '^(.*:)(\s+[^\[].*)$' '$1'(set_color blue)'$2'(set_color normal)
-end
--- functions/homebrew/brewup.fish
+++
@@ -1,3 +0,0 @@
-function brewup -d 'Update homebrew, upgrade installed packages, and cleanup'
-    brew update && brew upgrade && brew cleanup
-end
