2606cacb4b8038743122e901de7cac011965fdf9
mattmc3 <mattmc3@gmail.com>
2024-07-25 07:43:28 -0400

    Update local logic

--- conf.d/local.fish
+++ conf.d/local.fish
@@ -1,4 +1,8 @@
-if not test -f $__fish_config_dir/config.local.fish
+if test -r $__fish_config_dir/config.local.fish
+    source $__fish_config_dir/config.local.fish
+else
+    set -q DOTFILES
+    or set -Ux DOTFILES $HOME/.dotfiles
     set --local fishconfloc
     for fishconfloc in \
         "$DOTFILES.local/fish/conf.d/local.fish" \
@@ -7,7 +11,3 @@ if not test -f $__fish_config_dir/config.local.fish
         test -e $fishconfloc && ln -sf $fishconfloc $__fish_config_dir/config.local.fish
     end
 end
-
-test -r $__fish_config_dir/config.local.fish
-and source $__fish_config_dir/config.local.fish
-or return 1
