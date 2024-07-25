b19d9c96ad37dde32f6abdba578c9722df367fe9
mattmc3 <mattmc3@gmail.com>
2024-07-25 08:46:18 -0400

    Don't copy starship themes - use dotfiles

--- conf.d/prompt.fish
+++ conf.d/prompt.fish
@@ -7,6 +7,6 @@ set fish_greeting
 
 # Initialize starship.
 type -q starship || return 1
-set -q STARSHIP_CONFIG; or set -gx STARSHIP_CONFIG $__fish_config_dir/themes/mmc.toml
+set -q STARSHIP_CONFIG; or set -gx STARSHIP_CONFIG $HOME/.config/starship/mmc.toml
 cachecmd starship init fish | source
 enable_transience
