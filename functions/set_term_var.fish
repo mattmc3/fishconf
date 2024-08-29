12bfca24612d53ee41d1c0fcc3f06c4c13a82b99
mattmc3 <mattmc3@gmail.com>
2024-08-28 20:46:35 -0400

    Set current shell for WezTerm

---
+++ functions/set_term_var.fish
@@ -0,0 +1,14 @@
+# This function emits an OSC 1337 sequence to set a user var
+# associated with the current terminal pane.
+# It requires the `base64` utility to be available in the path.
+function set_term_var
+    if hash base64 2>/dev/null
+        if test -z "$TMUX"
+            printf "\033]1337;SetUserVar=%s=%s\007" $argv[1] (echo -n $argv[2] | base64)
+        else
+            # <https://github.com/tmux/tmux/wiki/FAQ#what-is-the-passthrough-escape-sequence-and-how-do-i-use-it>
+            # Note that you ALSO need to add "set -g allow-passthrough on" to your tmux.conf
+            printf "\033Ptmux;\033\033]1337;SetUserVar=%s=%s\007\033\\" $argv[1] (echo -n $argv[2] | base64)
+        end
+    end
+end
