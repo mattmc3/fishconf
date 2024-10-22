e819eb77c2195e42a2ef676f0c1065f845440b9c
mattmc3 <mattmc3@gmail.com>
2024-10-22 09:05:16 -0400

    Remove cachcmd

--- conf.d/prompt.fish
+++ conf.d/prompt.fish
@@ -8,7 +8,7 @@ set fish_greeting
 # Initialize starship.
 type -q starship || return 1
 set -gx STARSHIP_CONFIG $__fish_config_dir/themes/mmc.toml
-cachecmd starship init fish | source
+starship init fish | source
 enable_transience
 
 # Initialize hydro
