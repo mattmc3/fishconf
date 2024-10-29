d315b9060da2635f3afa7de53896947b5e56d5a4
mattmc3 <mattmc3@gmail.com>
2024-10-29 12:59:09 -0400

    Update brew

--- conf.d/brew.fish
+++ conf.d/brew.fish
@@ -1,3 +1,13 @@
+if not set -q HOMEBREW_PREFIX
+    if test -e /opt/homebrew/bin/brew
+        /opt/homebrew/bin/brew shellenv | source
+    else if test -e /usr/local/bin/brew
+        /usr/local/bin/brew shellenv | source
+    else
+        return 1
+    end
+end
+
 # Add keg-only apps
 set -q HOMEBREW_KEG_ONLY_APPS || set -U HOMEBREW_KEG_ONLY_APPS ruby curl sqlite
 for app in $HOMEBREW_KEG_ONLY_APPS
