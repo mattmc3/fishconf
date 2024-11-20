183b596a7541ab03a86d168aa8db90e038336a6c
mattmc3 <mattmc3@gmail.com>
2024-11-20 09:48:52 -0500

    Use plugins

--- functions/macos/rmdsstore.fish
+++
@@ -1,4 +0,0 @@
-function rmdsstore -d 'Remove .DS_Store files recursively in a directory'
-    test (count $argv) -gt 0 || set argv '.'
-    find "$argv" -type f -name .DS_Store -delete
-end
--- functions/magic-enter/magic-enter-cmd.fish
+++
@@ -1,7 +0,0 @@
-function magic-enter-cmd -d "Print the command to run when no command was given"
-    set --local cmd ls
-    if command git rev-parse --is-inside-work-tree &>/dev/null
-        set cmd "git status -sb"
-    end
-    echo $cmd
-end
