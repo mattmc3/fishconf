dd3107807eb934a6676d8613c6102f6ceb419145
mattmc3 <mattmc3@gmail.com>
2026-02-19 12:15:49 -0500

    Enhance terminal config

--- conf.d/terminal.fish
+++ conf.d/terminal.fish
@@ -3,11 +3,18 @@
 #
 
 switch (string lower $TERM_PROGRAM)
-    case wezterm
-        set_term_var TERM_CURRENT_SHELL "fish $FISH_VERSION"
+    case apple_terminal
+        set -gx SHELL_SESSIONS_DISABLE 1
+    case ghostty
+        if set -q GHOSTTY_RESOURCES_DIR
+            set -l _ghostty_int $GHOSTTY_RESOURCES_DIR/shell-integration/fish/ghostty-integration.fish
+            test -r "$_ghostty_int"; and source "$_ghostty_int"
+        end
     case vscode
         if type -q code
             set -l _vscode_shell_int (code --locate-shell-integration-path fish 2>/dev/null)
             test -f "$_vscode_shell_int"; and source "$_vscode_shell_int"
         end
+    case wezterm
+        set_term_var TERM_CURRENT_SHELL "fish $FISH_VERSION"
 end
