194e874010abd7032ba8e512a68b48802f005116
mattmc3 <mattmc3@gmail.com>
2026-05-06 10:59:26 -0400

    Update magic-enter

--- plugins/magic-enter/conf.d/magic-enter.fish
+++ plugins/magic-enter/conf.d/magic-enter.fish
@@ -8,3 +8,14 @@ function magic-enter --on-event prerun
     end
     commandline -f execute
 end
+
+if not functions -q magic-enter-cmd
+    function magic-enter-cmd --description "Print the command to run when no command was given"
+        test "$MAGIC_ENTER_ENABLED" -eq 1 || return 1
+        set -l cmd ls
+        if command git rev-parse --is-inside-work-tree &>/dev/null
+            set cmd "git status -sb"
+        end
+        echo $cmd
+    end
+end
