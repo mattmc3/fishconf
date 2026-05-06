6c0331d34812f79f75a655b7b09ba70914a39cda
mattmc3 <mattmc3@gmail.com>
2026-05-06 09:16:34 -0400

    Update fisher init

--- functions/init_fisher.fish
+++ functions/init_fisher.fish
@@ -2,24 +2,23 @@
 # Setup Fisher for plugins.
 #
 function init_fisher
-    set -q fisher_path || set -gx fisher_path $__fish_config_dir/.fisher
-    set -q my_plugins_path || set -gx my_plugins_path $__fish_config_dir/plugins
+    set -q fisher_path; or set -gx fisher_path $__fish_config_dir/.fisher
 
-    if test "$fisher_paths_initialized" != true
-        set --local idx (contains -i $__fish_config_dir/functions $fish_function_path || echo 1)
+    if not contains $fisher_path/functions $fish_function_path
+        set --local idx (contains -i $__fish_config_dir/functions $fish_function_path)
         set fish_function_path $fish_function_path[1..$idx] $fisher_path/functions $fish_function_path[(math $idx + 1)..]
+    end
 
-        set --local idx (contains -i $__fish_config_dir/completions $fish_complete_path || echo 1)
+    if not contains $fisher_path/completions $fish_complete_path
+        set --local idx (contains -i $__fish_config_dir/completions $fish_complete_path)
         set fish_complete_path $fish_complete_path[1..$idx] $fisher_path/completions $fish_complete_path[(math $idx + 1)..]
-
-        set -g fisher_paths_initialized true
     end
 
     if not test -d $fisher_path
         functions -e fisher &>/dev/null
         mkdir -p $fisher_path
         touch $__fish_config_dir/fish_plugins
-        curl -sL https://git.io/fisher | source
+        curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
         if test -s $__fish_config_dir/fish_plugins
             fisher update
         else
@@ -28,9 +27,8 @@ function init_fisher
     end
 
     for file in $fisher_path/conf.d/*.fish
-        if ! test -f $__fish_config_dir/conf.d/(path basename -- $file)
-            and test -f $file && test -r $file
-            builtin source $file
-        end
+        set --local base (path basename -- $file)
+        test -f $__fish_config_dir/conf.d/$base; and continue
+        test -r $file; and builtin source $file
     end
 end
