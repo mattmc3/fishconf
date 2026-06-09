a40e3a8302454e1b6e2abf8a98748ecd09912fc2
mattmc3 <mattmc3@gmail.com>
2026-06-09 13:16:04 -0400

    Remove unneeded func

--- functions/touchf.fish
+++
@@ -1,8 +0,0 @@
-function touchf --description 'Make parent dirs and touch a file'
-    if test (count $argv) -ne 1
-        echo >&2 "touchf: expected 1 argument"
-        return 1
-    end
-    test -f $argv[1]; and return 0
-    mkdir -p (path dirname $argv[1]); and touch $argv[1]
-end
