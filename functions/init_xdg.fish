690c5931f96f6fa43a4a3e9cbd089dcb25c0b0ca
mattmc3 <mattmc3@gmail.com>
2026-06-23 12:57:01 -0400

    Update runtime dir

--- functions/init_xdg.fish
+++ functions/init_xdg.fish
@@ -4,9 +4,7 @@ function init_xdg --description 'Set up XDG base directories'
     set -q XDG_STATE_HOME; or set -gx XDG_STATE_HOME $HOME/.local/state
     set -q XDG_CACHE_HOME; or set -gx XDG_CACHE_HOME $HOME/.cache
     if not set -q XDG_RUNTIME_DIR
-        set -l xdg_runtime_base /tmp
-        set -q TMPDIR; and set xdg_runtime_base $TMPDIR
-        set -gx XDG_RUNTIME_DIR (string trim -r -c / -- $xdg_runtime_base)/xdg-runtime-(id -u)
+        set -gx XDG_RUNTIME_DIR /tmp/xdg-runtime-(id -u)
     end
     for xdgdir in (path filter -vd $XDG_CONFIG_HOME $XDG_DATA_HOME $XDG_STATE_HOME $XDG_CACHE_HOME $XDG_RUNTIME_DIR)
         mkdir -p $xdgdir
